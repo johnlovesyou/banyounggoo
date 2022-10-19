8f1f52357804cde7cd21e8b4d08475e1865da20b
johnlovesyou <johnovesyou@naver.com>
2022-10-19 17:47:47 +0900

    basic-seting

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       반영구 홈페이지 입니다.
+       안녕하세요
+    </div>
+  );
+}
+
+export default App;
