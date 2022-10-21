62839e4d57a94c0ad96ee0ed8e916d7ea34ac2ff
johnlovesyou <johnovesyou@naver.com>
2022-10-21 13:18:40 +0900

    first

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import classnames from 'classnames';
+import { useState } from 'react'
+
 
 function App() {
+
   return (
     <div className="App">
        반영구 홈페이지 입니다.
        안녕하세요
+       <div>
+        
+        <select className='select'>
+          <option>1</option>
+          <option>2</option>
+        </select>
+
+     
+        
+        
+       </div>
     </div>
+
   );
 }
+
 
 export default App;
