ac323385f85c6d46dbc070f9fa78467f9ce7c996
johnlovesyou <johnovesyou@naver.com>
2022-10-21 13:50:02 +0900

    first

--- web.js
+++ web.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const app = express();
-app.use(express.static('build'));
+app.use(express.static(path.join(__dirname, '/build')));
 
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/build/index.html');
+  res.sendFile(__dirname + '/server/build/index.html');
 });
 
 app.listen(8001, function(){
   console.log('server is running')
+});
+
+
+
+
+app.get('/', function (요청, 응답) {
+  응답.sendFile(path.join(__dirname, '/server/build/index.html'));
+});
+app.get('*', function (요청, 응답) {
+  응답.sendFile(path.join(__dirname, '/server/build/index.html'));
 });
